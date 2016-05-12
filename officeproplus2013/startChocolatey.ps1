066cde575f7aaf7070f8955db8a809ae9c900561
iouri-s <iouri-s@users.noreply.github.com>
2016-05-11 22:04:52 -0700

    Files for office install

--- officeproplus2013/startChocolatey.ps1
+++ officeproplus2013/startChocolatey.ps1
@@ -14,18 +14,24 @@ Param(
 $secPassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
 
-$command = $file = $PSScriptRoot + "\ChocolateyPackageInstaller.ps1"
+#$command = $file = $PSScriptRoot + "\ChocolateyPackageInstaller.ps1"
 
-Enable-PSRemoting -Force -SkipNetworkProfileCheck
+#Enable-PSRemoting -Force -SkipNetworkProfileCheck
 
 # Ensure that current process can run scripts. 
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
 
 # Work around a bug in the Squirrel installer
-Invoke-Command -Credential $credential -ComputerName $env:COMPUTERNAME -ScriptBlock {
-    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force;
-    New-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify"  -Type directory -Force -ErrorAction Continue
-}
+#Invoke-Command -Credential $credential -ComputerName $env:COMPUTERNAME -ScriptBlock {
+#    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force;
+#    New-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify"  -Type directory -Force -ErrorAction Continue
+#}
 
-Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $packageList
-Disable-PSRemoting -Force
+$command2 = $PSScriptRoot + "\setup.exe"
+$config = $PSScriptRoot + "\configuration.xml"
+
+& $command2  /download $config
+& $command2  /configure $config
+
+#Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $packageList
+#Disable-PSRemoting -Force
